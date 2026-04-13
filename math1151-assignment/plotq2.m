a681199bf2c34c03dcc4ba0ad3b7c6bfcf7a1469
rocco <99860439+roccostager@users.noreply.github.com>
2026-04-13 23:38:20 +1000

    Improve plotting visuals

--- math1151-assignment/plotq2.m
+++ math1151-assignment/plotq2.m
@@ -1,12 +1,23 @@
-% Close previous figures (apparently this is best practice for plotting)
-close all;
+% Close previous figures
+close all;  % (apparently this is a best practice for plotting)
 
 % Declare anonymous function and interval
 f = @(x) exp(-2.*x) + 8*cos(74*x);
 xinterval = [0, pi/74];  % docs for fplot say xinterval is two-element vector [xmin, xmax]
 
-fplot(f, xinterval);  %fplot args are (anonymous func, xinterval[xmin, xmax])
-grid on;
+% Plot a red-dotted line on the x-axis
+fplot(0, xinterval, 'r--');
+hold on;  % This ensures the function can be plotted too!
 
+% Plotting the function itself
+fplot(f, xinterval, 'b');  %fplot args are (anonymous func, xinterval[xmin, xmax])
+
+% Circle the start and end points
+plot(0, 9, 'ro');
+plot(xinterval(2), f(xinterval(2)), 'ro');
+
+% Some aesthetic stuff
+grid on;
 txt = "f(x) = e^{-2x} + 8cos(74x)";
 title(txt);
+xlabel('x'); ylabel('f(x)');
